eab6e0daf9c5e11407e0ec92cfc5bed9664acdc4
Leo Molina <leonardo.molina.v@gmail.com>
2023-03-30 17:31:02 -0300

    added server = app.server to main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ app = dash.Dash(__name__,
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
+server = app.server
 
 app.layout = dbc.Container(
     [
