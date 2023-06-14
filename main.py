3382a5bd3a4add0f0f33c92e0bd5ceed5d882d92
Leo Molina <leonardo.molina.v@gmail.com>
2023-06-13 21:41:48 -0400

    added more info in the README

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ app = dash.Dash(__name__,
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
+
 server = app.server
 
 app.layout = dbc.Container(
