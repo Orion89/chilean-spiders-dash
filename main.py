1cc3f992dd44d966beb1b92ae24dbede90444277
Leo Molina <leonardo.molina.v@gmail.com>
2024-03-02 01:01:10 -0300

    added linkedin icon profile

--- main.py
+++ main.py
@@ -447,10 +447,17 @@ app.layout = dbc.Container(
                                     [
                                         html.P([
                                             html.A(
-                                                children=[html.I(className="bi bi-github")],
+                                                children=[html.I(className="bi bi-github", color='dark')],
                                                 disable_n_clicks=True,
                                                 href='https://github.com/Orion89',
                                                 title="GitHub profile"
+                                            ),
+                                            " ",
+                                            html.A(
+                                                children=[html.I(className="bi bi-linkedin")],
+                                                disable_n_clicks=True,
+                                                href='https://www.linkedin.com/in/leonardo-molina-v-68a601183/',
+                                                title="LinkedIn profile"
                                             ),
                                             " 2023 Leonardo Molina V."
                                             ]
